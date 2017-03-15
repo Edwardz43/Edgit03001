8120dfc74f07dfc1bb835d3df543bedcd7d10e62
EdLo <b93303015@gmail.com>
2017-03-15 22:15:10 +0800

    201703152215

--- EdJava/src/tw/org/iii/ed/Hw313_v2.java
+++ EdJava/src/tw/org/iii/ed/Hw313_v2.java
@@ -3,14 +3,19 @@ package tw.org.iii.ed;
 import javax.swing.JOptionPane;
 
 public class Hw313_v2 {
+	/*
+	 * 3/15 修改BUG :
+	 * 第一次輸入錯誤  再輸入正確密碼 
+	 * 但密碼卻停在第一次沒有儲存第二次正確的密碼
+	*/
 	
-	static boolean setPw(String password){
+	static String setPw(){ //改成回傳String
 		String limit = new String("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789$_*%?^");
 		// 1-1 測試輸入正確? 
 		//    預設3次  失敗就結束程式
 		for(int j =0;j<3;j++){
-			
 			//1-1-1檢查輸入字元正確?
+			String password = JOptionPane.showInputDialog("請輸入密碼:\n(6~20碼,字元限制a-z, A-Z, 0-9, 或$_*%?^等)");
 			int checkChar = 0;
 			for(int i=0;i<password.length();i++){
 				if(limit.indexOf(password.charAt(i))!=-1){  //用限制的字元去檢查輸入的密碼  只要不符合 checkChar就會++
@@ -21,20 +26,22 @@ public class Hw313_v2 {
 			//1-1-2 判斷格式正確?
 			//System.out.println(checkChar); debug用的
 			if(checkChar == password.length() && password.length()>=6 && password.length()<=20){
-				return true;
+				return password;//輸入正確 就回傳儲存的正確密碼
+			}else{
+				JOptionPane.showMessageDialog(null,"輸入錯誤!\n請輸入6~20碼,字元限制a-z, A-Z, 0-9, 或$_*%?^等");//輸入錯誤 重來(3次機會)
 			}
-			password = JOptionPane.showInputDialog("輸入錯誤!\n請輸入6~20碼,字元限制a-z, A-Z, 0-9, 或$_*%?^等");
 		}
-		return false;
+		return "ban";//多家一個回傳值   如果3次都錯誤  就回傳ban 等等會用到這個
 	}
 	
 	static boolean checkPw(String password){
 		//2-1 設定失敗次數  預設3次
+		String checkPw="";
 		int hist=0;
 		for(int i=0;i<3;i++){
 			
 			//2-2 檢查密碼 
-			String checkPw = JOptionPane.showInputDialog("確認密碼,請再次輸入:\n剩餘輸入次數:"+(3-hist));
+			checkPw = JOptionPane.showInputDialog("確認密碼,請再次輸入:\n剩餘輸入次數:"+(3-hist));
 			hist ++;  
 			if(checkPw.equals(password)){
 				return true; //正確 => 回傳 true
@@ -46,11 +53,11 @@ public class Hw313_v2 {
 	public static void main(String[] args) {
 		// Hw: password , 6~20, a-z A-Z 0-9 $_*%?^
 		// 1. 輸入密碼
-		String password= JOptionPane.showInputDialog("請輸入密碼:\n(6~20碼,字元限制a-z, A-Z, 0-9, 或$_*%?^等)");
-		boolean result = false;
-		result = setPw(password);
+		String password ="";
+		password = setPw();
 		
-		if(result){
+		//多加一個判斷  如果回傳的不是ban 就繼續下一個動作  若回傳ban  表示輸入3次皆失敗  程式直接結束
+		if(password!="ban"){
 			// 2.確認密碼
 			boolean check = false;
 			check = checkPw(password);
