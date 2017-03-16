b1bd5e35929785819f70b6bb2f0b749d9e069794
Mac <Mac@Mac-PC>
2017-03-16 16:28:48 +0800

    201703161628

--- EdJava/src/tw/org/iii/ed/Hw312_numToCheck.java
+++ EdJava/src/tw/org/iii/ed/Hw312_numToCheck.java
@@ -8,30 +8,30 @@ public class Hw312_numToCheck {
 	//所以用最原始的方法對付它=>手動更新例外
 	static String setZero(String convert){//輸入字串拼貼的結果  然後抓出不合中文語法的  
         String[] zero = {"零拾","零佰","零仟"}; 
-        String result = convert; 
         
         for (int i = 0;i < zero.length ;i++ )  
         {  
-            result = result.replace(zero[i], "零");//replace 顧名思義 就是取代  
+        	convert = convert.replace(zero[i], "零");//replace 顧名思義 就是取代  
         }
         //還有很多狀況  要自己跑測試來抓
-        result = result.replace("零零零零","");  
-        result = result.replace("零零零","零");  
-        result = result.replace("零零","零");  
-        result = result.replace("零萬","萬");  
-        result = result.replace("零圓","圓");
-        result = result.replace("億萬","億");
-        result = result.replace("零億","億");
-        return result;  
+        convert = convert.replace("零零零零","");  
+        convert = convert.replace("零零零","零");  
+        convert = convert.replace("零零","零");  
+        convert = convert.replace("零萬","萬");  
+        convert = convert.replace("零圓","圓");
+        convert = convert.replace("億萬","億");
+        convert = convert.replace("零億","億");
+        return convert;  
     }	
 	
 	public static void main(String[] args) {
-		String in =JOptionPane.showInputDialog("請輸入數字:");
+		String in =JOptionPane.showInputDialog("請輸入金額:");
 		System.out.println(in);//測試用
 		
 		//先建立字串拼貼需要的素材
 		String[] chNumber = {"零","壹","貳","参","肆","伍","陸","柒","捌","玖"};
-		String[] decimal = {"圓","拾","佰","仟","萬","拾","佰","仟","億","拾","佰","仟","兆"};
+		String[] decimal = {"圓","拾","佰","仟","萬","拾","佰","仟","億","拾","佰","仟"
+						    ,"兆","拾","佰","仟"};
 		
 		//用字串累加的概念來玩  先建立一個空的字串
 		String convert = "";
@@ -40,7 +40,8 @@ public class Hw312_numToCheck {
 		for(int i=0;i<in.length();i++){
 			int d = in.length()-i-1;
 			convert +=chNumber[in.charAt(i)-48]+decimal[d];	
-			//因為數字字元的ascii碼是從48開始，所以-48後才會顯示出想要的數字
+			                //將char手動轉成int 當作chNumber的索引值
+							//因為數字字元的ascii碼是從48開始，所以-48後才會顯示出想要的數字
 		}
 		
 		//把拼貼成果丟去檢查
