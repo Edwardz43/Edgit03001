6c19abaf437562ab79bf445f205d8cd782a451a7
EdLo <b93303015@gmail.com>
2017-04-10 15:24:02 +0800

    201704101523 DigitalSign v1.0

--- EdJava/src/tw/org/iii/ed/DigitalSign.java
+++ EdJava/src/tw/org/iii/ed/DigitalSign.java
@@ -5,26 +5,28 @@ import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.nio.channels.NetworkChannel;
 
 import javax.swing.JButton;
 import javax.swing.JColorChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 
 public class DigitalSign extends JFrame{
-	private JButton clear, undo, redo, color, save, open;
+	private JButton clear, undo, redo, color, stroke, BGC, save, open;
 	private MySignPanel msp;
 	
 	public DigitalSign(){
 		super("Digital Sign");
 		setLayout(new BorderLayout());
 		
-		clear= new JButton("Clear");undo= new JButton("undo");redo= new JButton("redo");
-		color =  new JButton("Color");
+		clear= new JButton("清空");undo= new JButton("undo");redo= new JButton("redo");stroke= new JButton("筆畫粗細");
+		color =  new JButton("設定顏色");BGC= new JButton("背景顏色");
 		JPanel top = new JPanel(new FlowLayout());
-		top.add(clear);top.add(undo);top.add(redo);top.add(color);
+		top.add(clear);top.add(undo);top.add(redo);top.add(color);top.add(stroke);top.add(BGC);
 		add(top, BorderLayout.NORTH);
-		
 		msp = new MySignPanel(this);
 		add(msp, BorderLayout.CENTER);
 		
@@ -35,14 +37,12 @@ public class DigitalSign extends JFrame{
 			}
 		});
 		undo.addActionListener(new ActionListener() {
-			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				msp.undo();
 			}
 		});
-		redo.addActionListener(new ActionListener() {
-			
+		redo.addActionListener(new ActionListener() {	
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				msp.redo();
@@ -50,21 +50,46 @@ public class DigitalSign extends JFrame{
 			}
 		});
 		
-		color.addActionListener(new ActionListener() {
-			
+		//換顏色的按鈕
+		color.addActionListener(new ActionListener() {		
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				Color newColor = JColorChooser.showDialog(DigitalSign.this, "Select a Color :", Color.BLUE);
+				Color myColor = JColorChooser.showDialog(DigitalSign.this, "Select a Color :", Color.BLUE);
+				msp.changeColor(myColor);
 				
 			}
 		});
 		
+		//換粗細的按鈕
+		stroke.addActionListener(new ActionListener() {	
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//用JOP來輸入粗細  加個try/catch避免輸入錯誤
+				try{
+					int  stroke = Integer.parseInt(JOptionPane.showInputDialog("輸入筆畫粗細 :"));
+					msp.changeStroke(stroke);
+				}catch(Exception ee){
+					JOptionPane.showMessageDialog(null, "請輸入正確數字!");
+				}
+			}
+		});
+		
+		//換背景顏色的按鈕
+		BGC.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Color bgc = JColorChooser.showDialog(DigitalSign.this, "Select a Color :", Color.yellow);
+				msp.changeBGC(bgc);
+			}
+		});
 		
 		setSize(640, 480);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	
+
 	public static void main(String[] args) {
 		new DigitalSign();
 	}
--- EdJava/src/tw/org/iii/ed/MySignPanel.java
+++ EdJava/src/tw/org/iii/ed/MySignPanel.java
@@ -12,18 +12,36 @@ import java.util.LinkedList;
 import javax.swing.JPanel;
 
 public class MySignPanel extends JPanel{
-	private DigitalSign ds;
 	private LinkedList<LinkedList<HashMap<String, Integer>>> lines, recycle;
-		
+	//每條線都有顏色與筆畫粗細  所以用HashMap存放很適合 
+	//要把線依序編號 方便之後repaint  所以外面再加一層LinkedList
+	private LinkedList<HashMap<String, Color>> color;
+	private LinkedList<HashMap<String, Integer>> stroke;
+	
+	//為了之後的方便  先宣告兩個全區變數  myColor mtStroke 並且給它們初始化
+	private Color myColor;
+	private int myStroke;
+	
 	public MySignPanel(DigitalSign ds){
 		setBackground(Color.yellow);
+		
+		//在這邊 先暫定藍色  粗細5
+		myColor =  Color.BLUE;
+		myStroke = 5;
 		
 		MyMouseListener listener = new MyMouseListener();
 		addMouseListener(listener);
 		addMouseMotionListener(listener);
+		myColor =  Color.BLUE;
+		myStroke = 5;
 		
 		lines = new LinkedList<>();
 		recycle = new LinkedList<>();
+		
+		//弄兩個新的  等等要拿來存color跟stroke
+		color = new LinkedList<>();
+		stroke = new LinkedList<>();
+
 		
 	}
 	// 1. mouse event, 2. data structure, 3. draw
@@ -33,23 +51,35 @@ public class MySignPanel extends JPanel{
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D)g;
 		
-		g2d.setColor();
-		
-		g2d.setStroke(new BasicStroke(15));
-		
-		for(LinkedList<HashMap<String, Integer>> line : lines){
-			//g2d.setColor(Color.black);
-			//g2d.setStroke(new BasicStroke(15));
+		//很重要 因為要決定每條線的粗細與顏色  需要每條線的編號(index)
+		//所以不用foreach 改用傳統的for 
+		for(int j=0; j<lines.size(); j++){
+			//叫出線的顏色
+			HashMap<String, Color> cOl0 = color.get(j);
+			g2d.setColor(myColor= cOl0.get("color"));
+			
+			//叫出粗細
+			HashMap<String, Integer> sOl0 = stroke.get(j);
+			g2d.setStroke(new BasicStroke(myStroke = sOl0.get("stroke")));
+			
+			//畫線
+			LinkedList<HashMap<String, Integer>> line = lines.get(j);
 			for(int i = 1 ; i<line.size(); i++){
 				HashMap<String, Integer> p0 = line.get(i-1);
 				HashMap<String, Integer> p1 = line.get(i);
-				int x0 = p0.get("x");int y0 = p0.get("y");
-				int x1 = p1.get("x");int y1 = p1.get("y");
-				g2d.drawLine(x0, y0, x1, y1);
-				//g2d.drawLine(p0.get("x"), p0.get("y"), p1.get("x"), p1.get("y"));
+				g2d.drawLine(p0.get("x"), p0.get("y"), p1.get("x"), p1.get("y"));
 			}
 		}
 				
+	}
+	
+	//變色
+	public void changeColor(Color color){
+		myColor=color;
+	}
+	//變粗細
+	public void changeStroke(int stroke) {
+		myStroke = stroke;	
 	}
 	
 	public void clear (){
@@ -70,8 +100,13 @@ public class MySignPanel extends JPanel{
 		}
 	}
 	
+	//變背景顏色
+	public void changeBGC(Color bgc) {
+		setBackground(bgc);
+	}
 	
-	
+	//很重要的來了  因為線的粗細跟顏色  是在你滑鼠按下的那一刻就決定了 
+	//所以我們在這邊記錄它的顏色根粗細
 	private class MyMouseListener extends MouseAdapter{
 		@Override
 		public void mousePressed(MouseEvent e) {
@@ -86,6 +121,16 @@ public class MySignPanel extends JPanel{
 			line.add(point);
 		
 			lines.add(line);
+			
+			//紀錄線的顏色
+			HashMap<String, Color> colorOfLine = new HashMap<>();
+			colorOfLine.put("color", myColor);
+			color.add(colorOfLine);
+			
+			//紀錄線的粗細
+			HashMap<String, Integer> strokeOfLine = new HashMap<>();
+			strokeOfLine.put("stroke", myStroke);
+			stroke.add(strokeOfLine);
 		}
 		
 		@Override
