cedfd2da8522753b39cbd1832a71ecfa316c9040
EdLo <b93303015@gmail.com>
2017-04-16 20:26:26 +0800

    201704162026 clock 1.0

--- EdJava/src/tw/org/iii/ed/PointerClock.java
+++ EdJava/src/tw/org/iii/ed/PointerClock.java
@@ -4,7 +4,6 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.util.LinkedList;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -18,69 +17,88 @@ public class PointerClock extends JPanel{
 	//LinkedList<Pointer> pointers;
 	
 	public PointerClock(){
+		//先預設視窗大小
 		setSize(800, 640);
+		//抓出銀幕的正中心點  把時鐘置中
+		viewW = getWidth()/2; viewH = getHeight()/2;
+		
+		timer = new Timer();
+		//三種針
 		secPointer = new Pointer();
 		minPointer = new Pointer();
 		hrPointer = new Pointer();
-		timer = new Timer();
-		//pointers= new LinkedList<Pointer>();
-		timer.schedule(secPointer, 1000 , 1000);
-		timer.schedule(minPointer, 1000 , 60*1000);
-		timer.schedule(hrPointer, 1000 , 60*60*1000);	
+		//v1版本  指針一次走0.6度  所以時間間格*10倍 讓指針有走很快的效果 
+		timer.schedule(secPointer, 1000 , 100);
+		timer.schedule(minPointer, 1000 , 60*100);
+		timer.schedule(hrPointer, 1000 , 60*60*100);	
 	}
 	
 	@Override
 	protected void paintComponent(Graphics g) {
 		Graphics2D g2d = (Graphics2D)g;
-		viewW = getWidth(); viewH = getHeight();
 		//System.out.println(viewW+"+"+viewH);
-		
 		//背景色
 		g2d.setColor(Color.gray);
-		g2d.fillRect(0, 0, viewW, viewH);
+		//填滿 記得*2
+		g2d.fillRect(0, 0, viewW*2, viewH*2);
 		
 		//畫圓
 		g2d.setStroke(new BasicStroke(10));
 		g2d.setColor(Color.black);
-		g2d.drawOval(viewW/2-200, viewH/2-200, 400, 400);
-		System.out.println(viewW/2+"+"+viewH/2);
+		//圓心位置要置中  半徑就和長的一半相等
+		g2d.drawOval(viewW-viewW/2, viewH-viewW/2, viewW, viewW);
 		
-		//刻度
+		//畫刻度
 		g2d.setStroke(new BasicStroke(10));
 		g2d.setColor(Color.black);
-	
+		
+		//主要的技巧和指針一樣  每6度就畫一個  長度我是選擇10
 		for(int i=0; i<60; i++){
-		g2d.drawLine(392 + (int)((Math.cos(Math.toRadians(i*6.0-90)))*200), 301 + (int)((Math.sin(Math.toRadians(i*6.0-90)))*200)
-				, 392 + (int)((Math.cos(Math.toRadians(i*6.0-90)))*180), 301 + (int)((Math.sin(Math.toRadians(i*6.0-90)))*180));
+			g2d.drawLine(viewW + cosPointer(i*10, viewW/2), viewH + sinPointer(i*10, viewW/2), 
+					viewW + cosPointer(i*10, viewW/2-10), viewH + sinPointer(i*10, viewW/2-10));
 		}
-		//指針
+		
+		//指針期時三根都一樣 只有timertask的地方會不同  之後改版會再修
 		//秒針
-		g2d.setStroke(new BasicStroke(5));
+		g2d.setStroke(new BasicStroke(3));
 		g2d.setColor(Color.red);
-		g2d.drawLine(viewW/2, viewH/2, secPointer.x, secPointer.y);
+		g2d.drawLine(viewW, viewH, secPointer.x, secPointer.y);
 		
 		//分針
-		g2d.setStroke(new BasicStroke(10));
+		g2d.setStroke(new BasicStroke(5));
 		g2d.setColor(Color.blue);
-		g2d.drawLine(viewW/2, viewH/2, minPointer.x, minPointer.y);
+		g2d.drawLine(viewW, viewH, minPointer.x, minPointer.y);
 		
 		//時針
-		g2d.setStroke(new BasicStroke(13));
+		g2d.setStroke(new BasicStroke(10));
 		g2d.setColor(Color.yellow);
-		g2d.drawLine(viewW/2, viewH/2, hrPointer.x, hrPointer.y);	
+		g2d.drawLine(viewW, viewH, hrPointer.x, hrPointer.y);	
+	}
+	
+	/*設兩個方法  輸入時間(n) 半徑(r)   就可以回傳指針移動的軌跡(x, y)
+	 * 主要就是高中數學的極坐標  任一點坐標可用(r*cosX, r*sinX)來表示  r是半徑  X是角度
+	 * 因為時鐘從12點開始算  和一般直角坐標不同  所以+90度
+	 * 但程式語言的作標和一般直角坐標上下顛倒  所以變成-90度(就是差180度的概念)
+	 * 又因為想讓指針有持續移動的感覺 所一次移動0.6度  也就是加快10倍
+	 */ 
+	public int cosPointer(int time, int r){
+		return (int)((Math.cos(Math.toRadians(time*0.6-90)))*r);
+	}
+	public int sinPointer(int time, int r){
+		return (int)((Math.sin(Math.toRadians(time*0.6-90)))*r);
 	}
 	
 	private class Pointer extends TimerTask{
 		private int x, y, n;	
 		Pointer(){
-			x=392; y=151; n=0; 
+			x=viewW; y=viewH; n=0; 
 		}
 		
+		//試作版的  暫時先設個計數器n 每跑一次就累加1 無線迴圈
 		@Override
 		public void run() {
-			x = 392 + (int)((Math.cos(Math.toRadians(n*6.0-90)))*150);
-			y = 301 + (int)((Math.sin(Math.toRadians(n*6.0-90)))*150);
-			System.out.println("x:"+x+","+"y:"+y+", n:"+n);
+			x = viewW + cosPointer(n, 150);
+			y = viewH + sinPointer(n, 150);
 			n++;
 			
 			repaint();
