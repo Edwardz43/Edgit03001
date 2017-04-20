ff03def3a1089d98aa0cb41e9053893d268dde1c
EdLo <b93303015@gmail.com>
2017-04-20 13:55:56 +0800

    201704201355 clock v1.2 update

--- EdJava/src/tw/org/iii/ed/PointerClock.java
+++ EdJava/src/tw/org/iii/ed/PointerClock.java
@@ -26,11 +26,12 @@ import javax.swing.JPanel;
 
 public class PointerClock extends JPanel{
 	private MyClockTask myclock;
-	private Timer timer;
-	private int halfW, halfH;
+	private BGTask bgtask;
+	private Timer timer, timerBG;
+	private int halfW, halfH, r, sR, mR, hR;
 	private int hh, mm, ss, sss;
-	private JLabel digiClock;
-	String number[] = { "12", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11" };
+	private String currentTime="", currentDate="", currentSec="";
+	private String number[] = { "12",  " 3", " 6",  " 9" };
 	
 	public PointerClock(){
 		setLayout(new BorderLayout());
@@ -40,14 +41,13 @@ public class PointerClock extends JPanel{
 		
 		//設置timer
 		timer = new Timer();
+		timerBG= new Timer();
 		myclock = new MyClockTask();
-		
-		//加一個label 等等放數位時鐘
-		digiClock = new JLabel();
-		add(digiClock, BorderLayout.SOUTH);
-		
+		bgtask = new BGTask();
+	
 		//因為是根據calendar的時間 所以間隔取多少是無所謂
-		timer.schedule(myclock, 1000 , 1);	
+		timer.schedule(myclock, 1000 , 100);
+		timerBG.schedule(bgtask, 0, 100);
 	}
 	
 	@Override
@@ -56,77 +56,101 @@ public class PointerClock extends JPanel{
 		//新增  讓時鐘跟著視窗縮放
 		halfW = getWidth()/2; halfH = getHeight()/2;
 		
-		//背景色
-		g2d.setColor(new Color(231, 204, 255));
-		//填滿 記得*2
-		g2d.fillRect(0, 0, halfW*2, halfH*2);
+		//新增:圓與指針的半徑  方便之後修改
+		r = Math.min(halfW*3/4,halfH*3/4);
+		sR = (int)(0.8*r);mR = (int)(0.6*r);hR = (int)(0.5*r);
 		
 		//畫圓
 		g2d.setStroke(new BasicStroke(5));
-		g2d.setColor(new Color(235, 117, 0));
+		g2d.setColor(new Color(57, 59, 167));
 		
 		//圓心位置要置中  
-		g2d.drawOval((int)(halfW-halfW/2), (int)(halfH-halfW/2), halfW, halfW);
+		g2d.fillOval(halfW-r, halfH-r, r*2, r*2);
+		
+		//新增: 日期
+//		g2d.setColor(new Color(255, 143, 87));
+//		g2d.setFont(new Font(null, Font.ITALIC, scaleParam(14)));
+//		g2d.drawString(currentDate, getX(354, r*0.6), getY(354, r*0.6));
+		
+		//新增: 數位時鐘
+		g2d.setColor(new Color(0, 235, 0));
+		g2d.setFont(new Font(null, Font.CENTER_BASELINE, scaleParam(14)));
+		g2d.drawString(currentTime, getX(240, r*0.3), getY(240, r*0.3));
+		g2d.setFont(new Font(null, Font.CENTER_BASELINE, scaleParam(16)));
+		g2d.drawString(currentSec, getX(230, r*0.6), getY(230, r*0.6));
 		
 		//畫刻度
 		g2d.setStroke(new BasicStroke(3));
-//		g2d.setColor(Color.black);
 		
 		//主要的技巧和指針一樣  每6度就畫一個  長度隨視窗大小變化
 		for(int i=0; i<60; i++){
 			if(i%5==0){
 				//5的倍數要長一點
-				g2d.drawLine(cosPointer(i*10, halfW/2.0), sinPointer(i*10, halfW/2.0), 
-						cosPointer(i*10, halfW*0.46), sinPointer(i*10, halfW*0.46));
+				g2d.setColor(new Color(0, 219, 0));
+				g2d.drawLine(getX(i*10, r*0.995), getY(i*10, r*0.995), getX(i*10, r*0.88), getY(i*10, r*0.88));
 			}else{
-				g2d.drawLine(cosPointer(i*10, halfW/2), sinPointer(i*10, halfW/2), 
-					cosPointer(i*10, halfW*0.48), sinPointer(i*10, halfW*0.48));
+				g2d.setColor(new Color(219, 0, 0));
+				g2d.drawLine(getX(i*10, r*0.995), getY(i*10, r*0.995), getX(i*10, r*0.96), getY(i*10, r*0.96));
 			}
 		}
 		
 		//畫數字
-		g2d.setFont(new Font(null, Font.ITALIC, 18));
-		g2d.setColor(new Color(65, 129, 129));
-		for(int i=0; i<12; i++){
+		g2d.setFont(new Font(null, Font.PLAIN, scaleParam(18)));
+		g2d.setColor(new Color(255, 143, 87));
+		for(int i=0; i<4; i++){
 			//微調位置  讓數字跟刻度對齊
-			g2d.drawString(number[i], cosPointer(i*50, halfW*0.4)-9, sinPointer(i*50, halfW*0.4)+7);
+			g2d.drawString(number[i], getX(i*150, r*0.7)-scaleParam(9), getY(i*150, r*0.7)+scaleParam(7));
 		}
 		
 		// 4/18:加上時分秒移動量   讓移動效果明顯
 		//時針
-		g2d.setStroke(new BasicStroke(6));
-		g2d.setColor(Color.green);
-		g2d.drawLine(halfW, halfH, cosPointer((hh+mm/60.0)*50, halfW*0.2), sinPointer((hh+mm/60.0)*50, halfW*0.2));	
+		g2d.setStroke(new BasicStroke(scaleParam(3)));
+		g2d.setColor(new Color(255, 238, 153));
+		g2d.drawLine(halfW, halfH, getX((hh+mm/60.0)*50, hR), getY((hh+mm/60.0)*50, hR));	
 		
 		//分針
-		g2d.setStroke(new BasicStroke(5));
-		g2d.setColor(Color.blue);
-		g2d.drawLine(halfW, halfH, cosPointer((mm+ss/60.0)*10, halfW*0.3), sinPointer((mm+ss/60.0)*10, halfW*0.3));
+		g2d.setStroke(new BasicStroke(scaleParam(3)));
+		//g2d.setColor(new Color(154, 76, 76));
+		g2d.drawLine(halfW, halfH, getX((mm+ss/60.0)*10, mR), getY((mm+ss/60.0)*10, mR));
 		
 		//秒針
-		g2d.setStroke(new BasicStroke(3));
-		g2d.setColor(Color.red);
-		g2d.drawLine(halfW, halfH, cosPointer(ss*10+sss*0.01, halfW*0.4), sinPointer(ss*10+sss*0.01, halfW*0.4));
-		//新增: 秒針長尾巴
-		g2d.drawLine(halfW, halfH, cosPointer((ss*10+sss*0.01)+300, halfW/9), sinPointer((ss*10+sss*0.01)+300, halfW/9));
+		g2d.setStroke(new BasicStroke(scaleParam(3)));
+		//g2d.setColor(new Color(154, 76, 76));
+		g2d.drawLine(halfW, halfH, getX(ss*10+sss*0.01, sR), getY(ss*10+sss*0.01, sR));
+		
+		//新增: 秒針長尾巴 
+		g2d.drawLine(halfW, halfH, getX((ss*10+sss*0.01)+300, sR/5), getY((ss*10+sss*0.01)+300, sR/5));
+		
+		//新增: 小小美化 火材棒頭
+		int tail2R=8; int tailR=tail2R/2;
+		g2d.setColor(new Color(248, 101, 93));
+		g2d.fillOval(getX((hh+mm/60.0)*50, hR)-scaleParam(tailR), getY((hh+mm/60.0)*50, hR)-scaleParam(tailR), scaleParam(tail2R), scaleParam(tail2R));
+		g2d.fillOval(getX((mm+ss/60.0)*10, mR)-scaleParam(tailR), getY((mm+ss/60.0)*10, mR)-scaleParam(tailR), scaleParam(tail2R), scaleParam(tail2R));
+		g2d.fillOval(getX(ss*10+sss*0.01, sR)-scaleParam(tailR), getY(ss*10+sss*0.01, sR)-scaleParam(tailR), scaleParam(tail2R), scaleParam(tail2R));
 				
 	}
 	
-	/*設兩個方法  輸入時間(n) 半徑(r)  就可以回傳指針移動的軌跡(x, y)
+	 /*設兩個方法  輸入時間(n) 半徑(r)  就可以回傳指針移動的軌跡(x, y)
 	 * 主要就是高中數學的極坐標  任一點坐標可用原點坐標加上(r*cosX, r*sinX)來表示  r是半徑  X是角度
 	 * 因為時鐘從12點開始算  和一般直角坐標不同  所以+90度
 	 * 但程式語言的作標和一般直角坐標上下顛倒  所以變成-90度(就是差180度的概念)
 	 * 4/18: 改成每次移動0.6度 讓移動效果更明顯    故所有變數都跟著放大10倍
-	 */ 
-	public int cosPointer(double time, double r){
-		return halfW+(int)((Math.cos(Math.toRadians(time*0.6-90)))*r);
+	 * 4/20: 微調公式 => cos(X-90)=sin(x)  sin(X-90)= -cos(X)  高中數學:三角函數-餘角公式
+	 */
+	
+	//4/20: 修改方法名稱: => getX, getY
+	public int getX(double time, double r){
+		return halfW+(int)((Math.sin(Math.toRadians(time*0.6)))*r);
 	}
-	public int sinPointer(double time, double r){
-		return halfH+(int)((Math.sin(Math.toRadians(time*0.6-90)))*r);
+	public int getY(double time, double r){
+		return halfH+(int)((Math.cos(Math.toRadians(time*0.6)))*r*-1);
+	}
+	
+	//4/20: 新增:scale參數  方便控制元件的大小
+	public int scaleParam(int n){
+		return (int)(n*Math.min(getWidth()/320.0 ,getHeight()/240.0));
 	}
 		
-	
-	//calendar拿來玩  
 	private class MyClockTask extends TimerTask{
 		@Override
 		public void run() {
@@ -134,26 +158,39 @@ public class PointerClock extends JPanel{
 			 * a:上下午   h, m, s: 時,分,秒
 			 * 然後用Date將其印出   功能和calendar類似
 			 */
-			SimpleDateFormat sdf = new SimpleDateFormat("a hh:mm:ss");
+			SimpleDateFormat sdf = new SimpleDateFormat(" H:mm");
+			SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy.MM.dd");
+			SimpleDateFormat sdf2 = new SimpleDateFormat("s");
 			Date current = new Date();
+			Date current2 = new Date();
+			Date current3 = new Date();
 			Calendar now = Calendar.getInstance();
 			hh =now.get(Calendar.HOUR);
 			mm = now.get(Calendar.MINUTE);
 			ss = now.get(Calendar.SECOND);;
 			sss = now.get(Calendar.MILLISECOND);
-			//旁邊印出數位時鐘
-			digiClock.setFont(new Font(null, Font.ROMAN_BASELINE, 18));
-			digiClock.setText(sdf.format(current));
+			//數位時鐘跟日期
+			currentTime = sdf.format(current);
+			currentDate = sdf1.format(current2);
+			currentSec = sdf2.format(current3);
 			repaint();
 		}
 	}
+	
+	private class BGTask extends TimerTask{
+		@Override
+		public void run() {
+			repaint();
+		}
+	}
+	
 	//秀出時鐘
 	public static void main(String[] args){
 		JFrame frame = new JFrame("Clock");
 		PointerClock clock = new PointerClock();
 		frame.add(clock);
 		frame.setVisible(true);
-		frame.setSize(320, 240);
+		frame.setSize(400, 300);
 		frame.setDefaultCloseOperation(3);
 	}
 }
