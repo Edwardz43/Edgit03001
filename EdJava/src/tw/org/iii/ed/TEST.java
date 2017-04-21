e26ba9cf87b44954d2c43cb471a17aa3f688b58b
EdLo <b93303015@gmail.com>
2017-04-22 01:38:43 +0800

    gg

--- EdJava/src/tw/org/iii/ed/TEST.java
+++ EdJava/src/tw/org/iii/ed/TEST.java
@@ -1,24 +1,9 @@
 package tw.org.iii.ed;
 
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import javax.swing.JFrame;
+public class TEST {
 
-public class TEST extends JFrame{
-	private PointerClock pointerclock;
-	private CalendarwithJFrame calendar;
-	private TEST(){
-		super("Pointer Clock");
-		setLayout(new BorderLayout());
-		pointerclock = new PointerClock();
-		add(pointerclock);
-		setSize(500, 300);
-		setVisible(true);
-		setDefaultCloseOperation(3);
+	public static void main(String[] args) {
+		new XOGame();
 	}
-    public static void main(String[] args) {
-    	new TEST();
-    }
- }
-
+}
 
