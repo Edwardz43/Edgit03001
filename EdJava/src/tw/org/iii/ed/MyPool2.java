c59f5eba24ade37c0f15ef9fd4f61214c3c6d314
user <who@where.com>
2017-04-27 18:05:43 +0800

    201704271805

--- EdJava/src/tw/org/iii/ed/MyPool2.java
+++ EdJava/src/tw/org/iii/ed/MyPool2.java
@@ -1,19 +1,22 @@
 package tw.org.iii.ed;
 
-import java.awt.BasicStroke;
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Shape;
-import java.awt.Stroke;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 public class MyPool2 extends JPanel{
 	private Ball ball;
@@ -22,23 +25,52 @@ public class MyPool2 extends JPanel{
 	private int viewW, viewH;
 	private LinkedList<Ball> balls;
 	private boolean isGameOver;
+	private long startTime, endTime;
+	private JPanel menuBar;
+	private JButton start;
+	private JRadioButton[] btns = new JRadioButton[3];
 	
 	public MyPool2(){
+		setLayout(new BorderLayout());
+		
+		menuBar = new JPanel();
+		menuBar.setLayout(new FlowLayout());
+		
+		start = new JButton("開始遊戲");
+		menuBar.add(start,FlowLayout.LEFT);
+		start.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				startgame();
+			}
+		});
+		
+//		for(int i=0;i<3; i++){
+//			btns[i] = new JRadioButton();
+//			btns[i].setText(""+(i+1));
+//			add(btns[i],FlowLayout.LEFT);
+//		}
+		add(menuBar, BorderLayout.NORTH);
 		ship = new Ship();
 		balls = new LinkedList<>();
 		timer = new Timer();
 		timer.schedule(new ViewTask(),0, 30);
-		timer.schedule(new createBall(), 0 , 80);
+		timer.schedule(new createBall(), 500 , 70);
 		timer.schedule(ship, 200, 10);
-		isGameOver = false;
+		isGameOver = true;
 		addMouseMotionListener(new MyMouseAdapter());
 	}
+	
 	private class MyMouseAdapter extends MouseAdapter{
 		@Override
 		public void mouseMoved(MouseEvent e) {
-			requestFocus();
-			ship.x = e.getX()-20; ship.y = e.getY()-20;
+				ship.x = e.getX()-20; ship.y = e.getY()-20;	
 		}
+	}
+	
+	protected void startgame() {
+		isGameOver = false;
+		startTime = System.currentTimeMillis();
 	}
 	
 	private class createBall extends TimerTask{
@@ -83,28 +115,37 @@ public class MyPool2 extends JPanel{
 	
 	@Override
 	protected void paintComponent(Graphics g) {
+		
 		Graphics2D g2d= (Graphics2D)g;
+		if(isGameOver){
+			g2d.setColor(Color.red);
+			g2d.setFont(new Font(null,Font.CENTER_BASELINE, 30));
+			g2d.drawString("Game Over !", viewW/2-100,viewH/2);
+			g2d.drawString("Record:"+endTime/1000.0, viewW/2-100,viewH/2+30);
+			
+		}else{
 		
 		viewW = getWidth(); viewH=getHeight();
 		
-		g2d.setColor(Color.black);
+		g2d.setColor(new Color(219, 255, 255));
 		g2d.fillRect(0, 0, viewW, viewH);
 		
-		g2d.setColor(Color.red);
+		g2d.setColor(new Color(91, 91, 174));
 		g2d.fillOval(ship.x, ship.y, 40, 40);
 		
-		g2d.setColor(Color.yellow);
+		g2d.setColor(new Color(107, 107, 107));
 		
 		try{
 			for(Ball ball : balls){
+				if(isGameOver){
+					ball.interrupt();
+				}
 				g2d.fillOval(ball.x, ball.y, 20, 20);
+				
 			}
 		}catch(Exception ee){}
-		if(isGameOver){
-			g2d.setColor(Color.red);
-			g2d.setStroke(new BasicStroke(18));
-			g2d.drawString("Game Over !", viewW/2-30,viewH/2);
 		}
+		
 	}
 	
 	private class Ball extends Thread{
@@ -122,11 +163,17 @@ public class MyPool2 extends JPanel{
 					x += dx;
 					y += dy;
 					
-					if(x+10 <= ship.x  && x - 10 >= ship.x && y +10 <= ship.y && y -10 >= ship.y ){
-						isGameOver = true;		 
+					if(x+10 >= ship.x  && x - 10 <= ship.x+10 && y +10 >= ship.y && y -10 <= ship.y+10 ){
+						isGameOver = true;
+						endTime = System.currentTimeMillis()- startTime;
+						
+						System.out.println("playTime:"+endTime/1000.0);
+						
+//						Thread.interrupted();
+//						break;
 					}
 					try {
-						Thread.sleep(60);
+						Thread.sleep(30);
 					} catch (InterruptedException e) {}
 					repaint();
 				}
@@ -140,8 +187,7 @@ public class MyPool2 extends JPanel{
 		
 		@Override
 		public void run() {
-			if(isGameOver == false){
-				
+			if(isGameOver == false){	
 				repaint();
 			}
 		}	
