be056f141e9876cc37091fa90fb6bd104906516c
EdLo <b93303015@gmail.com>
2017-05-23 01:47:32 +0800

    201705230146 leetcode accepted solution

--- EdJava/src/tw/org/iii/ed/ReverseWordsinaStringIII.java
+++ EdJava/src/tw/org/iii/ed/ReverseWordsinaStringIII.java
@@ -1,5 +1,7 @@
 package tw.org.iii.ed;
 
+import java.util.LinkedList;
+
 //557. Reverse Words in a String III
 //Given a string, you need to reverse the order of characters in each word within a sentence, 
 //while still preserving whitespace and initial word order.
@@ -14,20 +16,31 @@ package tw.org.iii.ed;
 public class ReverseWordsinaStringIII {
 
 	public static void main(String[] args) {
-		String s = "Let's take LeetCode contest";
-		StringBuffer temp = new StringBuffer();
-		temp.append(s.substring(0,s.length()));
-		temp.reverse();
-		StringBuffer result = new StringBuffer();
-		while(temp.indexOf(" ")>0){
-			result.insert(0, temp , 0, temp.indexOf(" "));
-			result.insert(0," ");
-			temp.delete(0, temp.indexOf(" ")+1);
+		String s = "This organization has no public members. You must be a member to see who’s a part of this organization";
+		long start = System.currentTimeMillis();
+		String[] temp = s.split("\\s");
+		StringBuffer temp2 = new StringBuffer();
+		String result = new String();
+		for(int i = 0; i< temp.length -1; i++){
+			temp2.append(temp[i]).reverse();
+			result = result.concat(temp2.toString() + " ");
+			temp2.delete(0, temp2.length());
 		}
-		result.insert(0, temp , 0, temp.length());	
+		temp2.append(temp[temp.length-1]).reverse();
+		result = result.concat(temp2.toString());
+		long end = System.currentTimeMillis() - start;
+		//return result;
 		
-			
-		System.out.println(temp);
+		
+		//result[0] = result[0].append(temp[0]);
+		
+		//LinkedList<String> result = new LinkedList();
+//		result.add(s);
+		
+//		for(String s1 : temp){
+//			
+//		}
+		System.out.println(end);
 		System.out.println(result);
 		
 	}
