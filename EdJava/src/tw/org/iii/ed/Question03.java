255ff8b25cd353ca58dd6e43fd6945cde671a791
Edlo <b93303015@gmail.com>
2017-09-26 16:31:26 +0800

    20170926 update

---
+++ EdJava/src/tw/org/iii/ed/Question03.java
@@ -0,0 +1,8 @@
+package tw.org.iii.ed;
+
+public class Question03 {
+	public static void main(String[] args) {	
+		char a = 'a', b = 'b';
+		System.out.println(a + b + "" + a + b);
+	}
+}
---
+++ EdJava/src/tw/org/iii/ed/Question04.java
@@ -0,0 +1,8 @@
+package tw.org.iii.ed;
+
+public class Question04 {
+	public static void main(String[] args) {	
+		//byte a = 1, b = 2, c = (byte)++a, d = (byte)b++, e = (byte)a + b;
+		//compile error
+	}
+}
